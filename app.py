cfe03131806d9ade7c6de353f1c1e80e28f9e3d7
StanStuh <stanko.stuhec@gmail.com>
2024-10-08 08:49:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,23 @@ if uploaded_file is not None:
         st.write("Cleaned Data (after dropping NaN values):")
         st.dataframe(cleaned_df)
 
+        # Create new charts for v1 and v2
+        st.subheader("Cleaned Data: Smoothed Speed (v1, v2) and Calculated Distance (s2)")
+        
+        # New Plot for Smoothed Speeds v1 and v2
+        fig_cleaned_speed = go.Figure()
+        fig_cleaned_speed.add_trace(go.Scatter(x=cleaned_df['t'], y=cleaned_df['v1'], mode='lines', name='Smoothed Speed (v1)', line=dict(color='orange')))
+        fig_cleaned_speed.add_trace(go.Scatter(x=cleaned_df['t'], y=cleaned_df['v2'], mode='lines', name='Smoothed Speed (v2)', line=dict(color='green')))
+        fig_cleaned_speed.update_layout(title="Smoothed Speeds (v1 and v2)", xaxis_title="Time (s)", yaxis_title="Speed (m/s)")
+        
+        st.plotly_chart(fig_cleaned_speed, use_container_width=True)
+
+        # New Plot for Calculated Distance (s2)
+        fig_cleaned_distance = go.Figure()
+        fig_cleaned_distance.add_trace(go.Scatter(x=cleaned_df['t'], y=cleaned_df['s2'], mode='lines', name='Calculated Distance (s2)', line=dict(color='red', dash='dash')))
+        fig_cleaned_distance.update_layout(title="Calculated Distance (s2)", xaxis_title="Time (s)", yaxis_title="Distance (m)")
+        
+        st.plotly_chart(fig_cleaned_distance, use_container_width=True)
+
     except Exception as e:
         st.error(f"Pri obdelavi datoteke je prišlo do napake: {e}")
